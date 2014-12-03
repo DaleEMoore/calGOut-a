9c4072b42d78a87fba64c3c6cfecae50146520b5
Dale E. Moore <DaleEMoore@gMail.Com>
2014-12-03 17:23:05 -0600

    get the start and end_date from tkinter

--- google_calendar_API_v3-1.py
+++ google_calendar_API_v3-1.py
@@ -167,11 +167,13 @@ __API_CLIENT_ID__ = '232867676714.apps.googleusercontent.com'
 __API_CLIENT_SECRET__ = '3tZSxItw6_VnZMezQwC8lUqy'
 
 # These are standard libraries and should never fail
+from Tkinter import * # Python2
 import sys, os, re, shlex, time, calendar
 import locale, textwrap, signal
 from Queue import Queue
 from datetime import datetime, timedelta, date
 from unicodedata import east_asian_width
+import traceback
 
 # Required 3rd partie libraries
 try:
@@ -222,12 +224,18 @@ def Usage(expanded=False):
         print FLAGS.MainModuleHelp()
     sys.exit(1)
 
-# TODO; build up fields for at-end output:
+# build up fields for at-end output:
 oCalendar = 'Calendar'
 oDate = '1/1/1001'
 oTime = '1:1:1'
 oDuration = 1
-oDescription = 'Description'
+oDescription = u'Description'
+start_date = '1/1/1001'
+end_date = '1/1/1001'
+search_string = u'bill'
+destination_file = "t1.csv"
+# TODO; build up fields for entry by operator:
+# TODO; pull tkinter from main.py then pass those fields into controlling this CSV execution.
 
 class CLR:
 
@@ -1159,13 +1167,16 @@ class gcalcli:
 
 
     def csvPrintEvent(self, event, prefix):
+        # build up fields for at-end output:                      Calendar, Date, Time, Duration, Description
         global oCalendar #= 'Calendar'
         global oDate #= '1/1/1001'
         global oTime #= '1:1:1'
         global oDuration #= 1
         global oDescription #= 'Description'
-
-        # TODO; build up fields for at-end output:                      Calendar, Date, Time, Duration, Description
+        global start_date
+        global end_date
+        global search_string
+        global destination_file #= "t1.csv"
 
         def _formatDescr(descr, indent, box):
             wrapper = textwrap.TextWrapper()
@@ -1297,7 +1308,7 @@ class gcalcli:
                 )
             #PrintMsg(CLR_NRM(), xstr)
 
-        # TODO; fill in oCalendar, oDate, oTime, oDuration, oDescription
+        # fill in oCalendar, oDate, oTime, oDuration, oDescription
         #print(dir(event))
         #Calendar = event.gcalcli_cal['summary']
         # UnboundLocalError: local variable 'oCalendar' referenced before assignment
@@ -1319,18 +1330,31 @@ class gcalcli:
         # per http://stackoverflow.com/questions/2609259/converting-string-to-datetime-object-in-python
         ds = parse(ds1)
         de = parse(de1)
-        # TODO; is event['creator']['displayName'] == name of calendar?
+        # is event['creator']['displayName'] == name of calendar?
         # Or use ListAllCalendars()? to get calendars' name.
-        # TODO; fill in oCalendar, oDate, oTime, oDuration, oDescription
+        # fill in oCalendar, oDate, oTime, oDuration, oDescription
         oCalendar = event['creator']['displayName']
         oDate = ds.date()
         oTime = ds.time()
         oDuration = de - ds
         oDescription = event['summary']
-        sys.stdout.write (oCalendar + "," + str(oDate) + "," + str(oTime) + "," + str(oDuration) + "," + str(oDescription) + '\n')
-        #sys.stdout.write (oCalendar + "," + str(event['start']) + "," + str(event['end']) + "," + str(event['summary']) + '\n')
-        #PrintMsg(CLR_BLK(), Calendar + "," + str(Date) + "," + str(Time) + "," + str(Duration) + "," + Description)
-        #print oCalendar, oDate, oTime, oDuration, oDescription
+        # does oDescription contain search_string?
+        if search_string.upper() in oDescription.upper():
+            # TODO; does this event exist in the start_date and end_date range?
+            # can't compare offset-naive and offset-aware datetimes
+            # And once I fix the offset-naive versus offset-aware datetimes comparison...
+            # What's the math for events that pass through my start_date and end_date?
+            if start_date.replace(tzinfo=None) <= ds.replace(tzinfo=None) <= end_date.replace(tzinfo=None):
+                    #or start_date <= de <= end_date
+                    #or ds <= start_date <= de
+                    # ds <= end_date <= de
+                sys.stdout.write (oCalendar + "," + str(oDate) + "," + str(oTime) + "," + str(oDuration) + "," + str(oDescription) + '\n')
+                fOut = open(destination_file, 'a')
+                fOut.write(oCalendar + "," + str(oDate) + "," + str(oTime) + "," + str(oDuration) + "," + str(oDescription) + '\n')
+                fOut.close()
+                #sys.stdout.write (oCalendar + "," + str(event['start']) + "," + str(event['end']) + "," + str(event['summary']) + '\n')
+                #PrintMsg(CLR_BLK(), Calendar + "," + str(Date) + "," + str(Time) + "," + str(Duration) + "," + Description)
+                #print oCalendar, oDate, oTime, oDuration, oDescription
 
 
 
@@ -1641,6 +1665,7 @@ class gcalcli:
                                          microsecond=0)
         else:
             try:
+                #start = startText # Bad Request
                 start = self.dateParser.fromString(startText, not self.ignoreStarted)
             except:
                 PrintErrMsg('Error: failed to parse start time\n')
@@ -1650,6 +1675,7 @@ class gcalcli:
             end = (start + timedelta(days=self.agendaLength))
         else:
             try:
+                #end = endText # Bad Request
                 end = self.dateParser.fromString(endText, not self.ignoreStarted)
             except:
                 PrintErrMsg('Error: failed to parse end time\n')
@@ -2312,8 +2338,11 @@ def BowChickaWowWow():
 
     elif args[0] == 'agenda':
         if len(args) == 3: # start and end
+            # TODO; get the start_date from tkinter
+            #gcal.AgendaQuery(startText=start_date, endText=end_date)
             gcal.AgendaQuery(startText=args[1], endText=args[2])
         elif len(args) == 2: # start
+            #gcal.AgendaQuery(startText=start_date)
             gcal.AgendaQuery(startText=args[1])
         elif len(args) == 1: # defaults
             gcal.AgendaQuery()
@@ -2326,6 +2355,8 @@ def BowChickaWowWow():
 
     elif args[0] == 'csv':
         if len(args) == 3: # start and end
+            # TODO; get the end_date from tkinter
+            #gcal.AgendaQuery(startText=start_date, endText=end_date)
             gcal.AgendaQuery(startText=args[1], endText=args[2])
         else:
             PrintErrMsg('Error: invalid csv arguments\n')
@@ -2446,6 +2477,116 @@ def BowChickaWowWow():
             PrintErrMsg('Error: invalid import arguments\n')
             sys.exit(1)
 
+def makeent(root, field, showAss=False):
+    row = Frame(root)
+    # TODO; if name includes "Date" make it a date field. tkinter doesn't have date picker I might roll my own.
+    # TODO; if name is "Message" make it multi-line.
+    #user = makeentry(parent, "User name:", 10)
+    #password = makeentry(parent, "Password:", 10, show="*")
+    lab = Label(row, width=22, text=field+": ", anchor='w')
+    if showAss:
+        ent = Entry(row, width=50, show='*')
+    else:
+        ent = Entry(row, width=50)
+    #s1 = "Entry(row, width=50" + entryOpt + ")"
+    #s2 = exec("print(s1)")
+    #ent = exec(s1)
+    #ent = Entry(row, width=50)
+    ent.insert(0,"")
+    row.pack(side=TOP, fill=X, padx=5, pady=5)
+    lab.pack(side=LEFT)
+    ent.pack(side=RIGHT, expand=YES, fill=X)
+    return ent
+
+def update_status(entries, status_description):
+    entries['Status'].delete(0,END)
+    entries['Status'].insert(0, status_description )
+    print("Status: %s" % status_description)
+
+def update_message(entries, message_description):
+    entries['Message'].delete(0,END)
+    entries['Message'].insert(0, message_description)
+    print("Message: %s" % message_description)
+
+def get_events(entries):
+    global start_date
+    global end_date
+    global search_string
+    global destination_file #= "t1.csv"
+
+    update_status(ents, "Processing...")
+    # TODO; status isn't updated until this is done, this should be multiprocessing?
+    # Like: http://stackoverflow.com/questions/16699682/python-tkinter-status-bar-not-updating-correctly
+    try:
+        # get values from user
+        account = entries['Google Account'].get()
+        password = entries['Google Account Password'].get()
+        #show_password = entries['Show Password'].get()
+        d1 = entries['Start Date'].get()
+        start_date = datetime.strptime(d1, '%Y-%m-%d')
+        #start_date = datetime.datetime.strptime(d1, '%Y-%m-%d')
+        #start_date = datetime.datetime.strptime(d1, '%m/%d/%Y')
+        d1 = entries['End Date'].get()
+        # change end_date to day+1 or time to 23:59:59
+        end_date = datetime.strptime(d1, '%Y-%m-%d')+timedelta(days=1)
+        #end_date = datetime.datetime.strptime(d1, '%Y-%m-%d')+datetime.timedelta(days=1)
+        #end_date = datetime.datetime.strptime(d1+datetime.timedelta(days=1), '%Y-%m-%d')
+        #end_date = datetime.datetime.strptime(d1, '%m/%d/%Y')
+        search_string = unicode(entries['Search String'].get())
+        destination_file = entries['Destination File'].get()
+        print (account, start_date, end_date, search_string, destination_file)
+        #print (account, password, show_password, start_date, end_date, search_string, destination_file)
+
+        BowChickaWowWow()
+
+        # validate date fields since I don't have a date picker yet.
+        # get events from Google
+        #token = google_calendar_fetcher.login(account,password)
+        #try:
+        #    google_calendar_fetcher.get_calendars(token, start_date, end_date, search_string, destination_file)
+        #    # filter events by Start Date, End Date and Search String.
+        #    google_calendar_fetcher.printOut(destination_file)
+        #   #google_calendar_fetcher.print_output()
+        #except:
+        #    exc_type, exc_value, exc_traceback = sys.exc_info()
+        #    update_message(entries, exc_value)
+        #    traceback.print_exc()
+
+        #client = gdata.calendar.client.CalendarClient(source='yourCo-yourAppName-v1')
+        #client.ClientLogin(account, password, client.source)
+        #PrintUserCalendars(client)
+        # TODO; show events to user
+        pass
+    except:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        update_message(entries, exc_value)
+        traceback.print_exc()
+        #print exc_type
+        #print exc_value
+        #print exc_traceback
+    update_status(ents, "Waiting for entry...")
+
+def makeform(root):
+    account_password_dates_fields = ('Status',
+        'Message',
+        'Start Date',
+        'End Date',
+        'Search String',
+        'Destination File',
+        'Google Account',
+    )
+    #    'Google Account Password',
+    entries = {}
+    for field in account_password_dates_fields:
+        entries[field] = makeent(root, field)
+    s1 = 'Google Account Password'
+    entries[s1] = makeent(root,s1, showAss=True)
+    return entries
+
+def func(event):
+    #print("You hit return.")
+    get_events(ents)
+
 def SIGINT_handler(signum, frame):
     PrintErrMsg('Signal caught, bye!\n')
     sys.exit(1)
@@ -2460,4 +2601,75 @@ if __name__ == '__main__':
     # Check date ranges; '2014-11-24', '2014-11-30' got 22nd through 29th
     print (sys.argv[0], 'csv', str(dS), str(dE)) # Force csv output.
     CLR.useColor = False # Don't output any colors.
-    BowChickaWowWow()
+
+    # TODO; Is there a button accelerator key for tkinter buttons?
+    #       I was not able to get Alt-G or Alt-Q to bind. Something funny is going on.
+    #test1=raw_input("gimme something")
+    #test2=raw_input("gimme more")
+    root = Tk()
+    ents = makeform(root)
+    #root.bind('<Return>', (get_events(root)))
+    # Catch ENTER from form and don't generate an error.
+    root.bind('<Return>', func)
+    # ENTER goes to tkinter.__init__.CallWrapper.__call__ when not bound to something.
+    #root.bind('<Return>', (lambda event, e=ents: e.get()))
+    #root.bind('<Return>', (lambda event, e=ents: fetch(e)))
+    update_status(ents, "Starting...")
+    b1 = Button(root, text='Get Events', command=(lambda e=ents: get_events(e)))
+    #b1 = Button(root, text='Get Events', command=(lambda e=ents: get_events(e)), underline=0)
+    b1.pack(side=LEFT, padx=5, pady=5)
+    #root.bind('<Alt-G>', func1)
+    #root.bind('<Shift-G>', func1)
+    #root.bind('<Control-G>', func1)
+    #b2 = Button(root, text='Monthly Payment', command=(lambda e=ents: monthly_payment(e)))
+    #b2.pack(side=LEFT, padx=5, pady=5)
+    b3 = Button(root, text='Quit', command=root.quit)
+    #b3 = Button(root, text='Quit', command=root.quit, underline=0)
+    b3.pack(side=LEFT, padx=5, pady=5)
+    #root.bind('<Alt-Q>', func2)
+    update_status(ents, "Waiting for entry...")
+    update_message(ents, "Enter dates as yyyy-mm-dd!")
+    # TODO; figure out a way to keep the Google account password secret.
+    s1 = """
+          google pycharm security passwords
+            python master password database
+                http://stackoverflow.com/questions/12042724/securely-storing-passwords-for-use-in-python-script
+                http://stackoverflow.com/questions/7014953/i-need-to-securely-store-a-username-and-password-in-python-what-are-my-options
+                https://docs.python.org/2/library/getpass.html
+                https://charlesleifer.com/blog/creating-a-personal-password-manager/
+            python Creating a personal password manager
+            PyCharm password database
+        Something creates a sqlite db and that could be my answer if encrypted.
+            Django project.
+    """
+    ents['Google Account'].delete(0,END)
+    ents['Google Account'].insert(0, "DaleEMoore")
+    #ents['Google Account'].insert(0, "MooreWorksService")
+    ents['Google Account Password'].delete(0,END)
+    #ents['Google Account Password'].insert(0,END, "password")
+    #ents['Show Password'].delete(0,END)
+    #ents['Show Password'].insert(0, "No")
+    ents['Start Date'].delete(0,END)
+    # Start Date endDate - 6 days.
+    ents['End Date'].delete(0,END)
+
+    # End Date is yesterday.
+    dE = date.today() - timedelta(days=1)
+    dS = dE - timedelta(days=6)
+    #dE = datetime.date.today() - datetime.timedelta(days=1)
+    #dow = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    ##       0         1          2            3           4         5           6
+    ## date.weekday(); Monday is 0 and Sunday is 6
+    #dS = dE - datetime.timedelta(days=6)
+    ents['End Date'].insert(0, dE)
+    ents['Start Date'].insert(0, dS)
+    ents['Search String'].delete(0,END)
+    ents['Search String'].insert(0, "Bill")
+    ents['Destination File'].delete(0,END)
+    ents['Destination File'].insert(0, "t1.csv")
+
+    ents['Google Account Password'].focus()
+
+    root.mainloop()
+
+    #BowChickaWowWow()
