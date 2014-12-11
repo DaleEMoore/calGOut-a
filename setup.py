421e3d6a12a798ec07bd4c5186627d12194384dd
Dale E. Moore <DaleEMoore@gMail.Com>
2014-12-11 06:08:31 -0600

    TrySetup git branch first push

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='calGOut',
+    version='0.1',
+    packages=['old'],
+    url='MooreWorks.Net',
+    license='License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+    author='Dale E. Moore',
+    author_email='DaleEMoore@gMail.Com',
+    description='Extract Google Calendar Events'
+)
